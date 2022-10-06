a7fdaaf684545f308165f55d955b52502619cec0
* <frabby983@gmail.com>
2022-10-06 09:18:14 +0600

    Arrays

--- main.c
+++ main.c
@@ -5,27 +5,41 @@ Author: Fazle Rabbi
 Date  : 06 October, 2022
 */
 
-int factorial(number)
-{
-   if(number == 0 || number == 1){
-      return 1;
-   }
-   else{
-      return (number * factorial(number - 1));
-      /*
-         number = 3;
-         3 * factorial(2);
-         3 * 2 * factorial(1);
-         3 * 2 * 1; --> Since, if condition exist!
-      */
-   }
-}
-
 int main()
 {  
-   int r;
-   r = factorial(3);
-   printf("The result is = %d", r);
+   // // Basic Array:
+   int marks[4];
+   marks[0] = 50;
+   marks[1] = 60;
+   marks[2] = 70;
+   marks[3] = 80;
+   
+   // printf("%d", marks[3]);
+   
+   // 1D Array:
+   int marks2[3] = {10,20,40};
+   for (int i = 0; i < 3; i++) {
+      printf("The value of element %d is = %d\n", i,marks2[i]);
+   }
+   
+   printf("\n");
+   
+   // 2D Array:
+   int results[6][4] = {
+      {66,67,78,99},
+      {76,27,48,69},
+      {76,27,48,69},
+      {76,27,48,69},
+      {76,27,48,69},
+      {76,27,48,69},
+   };
+   for (int i = 0; i < 6; i++) {
+      for (int j = 0; j < 4; j++) {
+         printf(" %d", results[i][j]);
+      }
+      printf("\n");
+   };
+   
    // End of program
    printf("\n");
    return 0;
