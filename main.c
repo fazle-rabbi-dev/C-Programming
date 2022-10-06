d6f916a6ee2ca7e25227030642a9c952046218a4
* <frabby983@gmail.com>
2022-10-06 06:14:00 +0600

    goto statement

--- main.c
+++ main.c
@@ -2,13 +2,22 @@
 
 int main()
 {  
-   int a;
-   int b;
-   printf("Enter number a: \n");
-   scanf("%d",&a);
-   printf("Enter number b: \n");
-   scanf("%d",&b);
-   printf("Result = %d",a+b);
+   int a = 0;
+   int num;
+   for (int i = 0; i < 4; i++) {
+      printf("%d\n", i);
+      for (int j = 0; j < 4; j++) {
+         printf("Enter 0 for exit: ");
+         scanf("%d", &num);
+         printf("You have entered %d\n", num);
+         if(num == 0){
+            // goto end;
+            break;
+         }
+      }
+   }
+   end:
+   // End of program
    printf("\n");
    return 0;
 }
