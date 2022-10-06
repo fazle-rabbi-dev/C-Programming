de399cde8eb2426169cc5a55976e7010cec35d37
* <frabby983@gmail.com>
2022-10-06 14:43:44 +0600

    Getting user input as string

--- main.c
+++ main.c
@@ -1,28 +1,11 @@
 #include <stdio.h>
 
-void sayHello();
-
 int main()
 {
-   // Easy Way of Creating Strings:
-   char text[] = "Hello World";
-   text[0] = 'J';
-   printf("%s\n", text);
-   
-   
-   // Another Way of Creating Strings:
-   char greetings[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
-   printf("%s\n", greetings);
-   printf("%d\n", sizeof(greetings)); 
-   
-   sayHello();
-   
+   // creating a string...
+   char name[30];
+   printf("Type your name: ");
+   scanf("%s", name);
+   printf("Hey %s, welcome to my awesome program 😍\n.", name);
    return 0;
 }
-
-
-void sayHello(){
-   char name[] = "Karim";
-   printf("Hello %s. How are you?\n", name);
-}
-   
