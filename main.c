2e7a4246e236fb57e5af2d35b4306092f5904b87
* <frabby983@gmail.com>
2022-10-07 11:00:38 +0600

    Arrays as argument

--- main.c
+++ main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 
-void changeValue(int *a, int *b)
+void demo(int arr[])
 {
-   int tmp = *a;
-   *a = *a + *b;
-   *b = tmp - *b;
+   // printf("%d\n", arr[0]);
+   for (int i = 0; i < 3; i++) {
+      printf("%d\n", arr[i]);
+   }
+   arr[0] = 99;
+}
+
+void demo2(int* array,int* a)
+{
+   printf("%p\n",array);
+   printf("%p\n",a);
+   for (int i = 0; i < 9; i++) {
+      printf("Value is = %d\n", array[i]);
+   }
 }
 
 int main()
 {
-   int a = 4,b = 3;
-   printf("The value of a is = %d\n", a);
-   printf("The value of b is = %d\n", b);
-   changeValue(&a, &b);
-   printf("The value of a is = %d\n", a);
-   printf("The value of b is = %d\n", b);
+   int arr[] = {10,20,30,40,50,60,70,80,90};
+   // printf("Before the value at index 0: %d\n", arr[0]);
+   // demo(arr);
+   // printf("After the value at index 0: %d\n", arr[0]);
+   int a = 67;
+   demo2(arr,&a);
    return 0;
 }
