e8079c1404d93a9049566f51fc79d8707017edb3
* <frabby983@gmail.com>
2022-10-08 09:05:29 +0600

    Typedef

--- main.c
+++ main.c
@@ -1,40 +1,21 @@
 #include <stdio.h>
 
-void swap(int a,int b)
+typedef struct StudentInfo
 {
-   printf("Before the value of a is: %d\n", a);
-   printf("Before the value of b is: %d\n", b);
-   printf("\n");
-   int temp = a; // 10
-   a = b; // 10
-   b = temp;
-   printf("\n");
-   printf("After the value of a is: %d\n", a);
-   printf("After the value of b is: %d\n", b);
-}
-
-void arrRev(int arr[])
-{
-   // 40,30,20,10
-   int temp;
-   for (int i = 0; i < 4/2; i++) {
-      temp = arr[i]; // 40,30
-      arr[i] = arr[3-i]; // 
-      arr[3-i] = temp;
-   }
-}
+  int id;
+  int rool;
+} SI;
 
 int main(){
-   // Swaping Variable:
    int a = 10;
-   int b = 99;
-   swap(a,b);
+   typedef int integer;
+   integer b = 20;
+   printf("The value of b is: %d\n", b);
+   printf("\n");
    
-   int arr[] = {10,20,30,40};
-   // printf("%d\n", arr[0]);
-   arrRev(arr);
-   for (int i = 0; i < 4; i++) {
-      printf("%d\n", arr[i]);
-   }
+   // 
+   SI s1;
+   s1.id = 100;
+   printf("Id of student is %d\n", s1.id);
    return 0;
 };
