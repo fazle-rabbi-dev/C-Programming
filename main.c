97c622564b01c526b9d0535b46cdeb8e90d31662
* <frabby983@gmail.com>
2022-10-08 19:13:06 +0600

    Static variable

--- main.c
+++ main.c
@@ -1,46 +1,26 @@
 #include <stdio.h>
-#include <string.h>
+#define PI = 3.14;
 
-void parser(char *html)
-{
-   // printf("%s\n", html);
-   int in = 0;
-   int index = 0;
-   for (int i = 0; i < strlen(html); i++) {
-      if(html[i] == '<'){
-         in = 1;
-         continue;
-      }
-      else if(html[i] == '>'){
-         in = 0;
-         continue;
-      }
-      if(in == 0){
-         html[index] = html[i];
-         index++;
-      }
-   }
-   html[index] = '\0';
-   
-   while(html[0] == ' '){
-      for (int i = 0; i < strlen(html); i++) {
-         // Left shifting:
-         html[i] = html[i+1];
-      }
-   }
-   
-   while(html[strlen(html) - 1] == ' '){
-      for (int i = 0; i < strlen(html); i++) {
-         html[strlen(html) - 1] = 'p';
-      }
-   }
-   
-   printf("||%s||", html);
+int a = 990; 
+
+int demo(int a1)
+{  
+   static int tmp = 0;
+   printf("Before tmp is: %d\n", tmp);
+   tmp ++;
+   return tmp;
 }
 
-int main(){
-   char html[] = "<h1>  Hello World  </h1>";
-   parser(html);
-   printf("\n");
+int main()
+{
+   int a = 10;
+   int b = 10;
+   printf("%d\n", a);
+   int test = demo(a);
+   test = demo(a);
+   test = demo(a);
+   test = demo(a);
+   test = demo(a);
+   // static int x = demo(a); // --> Throw error
    return 0;
 };
