67b0ba6356514f9fb68120e73c060386c7686649
* <frabby983@gmail.com>
2022-10-08 06:20:44 +0600

    Type casting

--- main.c
+++ main.c
@@ -1,27 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void demo(char *marks)
-{
-   printf("%x\n", (int)marks);
-   for (int i = 0; i < strlen(marks); i++) {
-      printf("%d\n", *(marks+i));
-   }
-   *marks = 99;
-   *(marks+1) = 70;
-}
-
 int main(){
-   char marks[] = {10,20,30,40,50};
-   demo(marks);
-   printf("\n");
-   printf("%d\n", marks[0]);
-   printf("%d\n", marks[1]);
-   printf("\n");
-   printf("\n");
-   
-   for (int i = 0; i < strlen(marks); i++) {
-      printf("The value = %d\n", marks[i]);
-   }
+   int a = 10;
+   float b = 30.6;
+   printf("The result = %d\n", (int)b);
    return 0;
 };
