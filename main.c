3d2c34a307b5c828fc81075e0d95851a4e0893ea
* <frabby983@gmail.com>
2022-10-10 10:17:24 +0600

    Dynamic memory

--- main.c
+++ main.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
-#define PI = 3.14;
+#include <stdlib.h>
 
-int a = 990; 
-
-int demo(int a1)
-{  
-   static int tmp = 0;
-   printf("Before tmp is: %d\n", tmp);
-   tmp ++;
-   return tmp;
-}
-
-int main()
-{
-   int a = 10;
-   int b = 10;
-   printf("%d\n", a);
-   int test = demo(a);
-   test = demo(a);
-   test = demo(a);
-   test = demo(a);
-   test = demo(a);
-   // static int x = demo(a); // --> Throw error
+int main(){
+   int *ptr;
+   // Use of malloc:
+   // ptr = (int*) malloc(3*sizeof(int));
+   // for (int i = 0; i < 3; i++) {
+   //    printf("Enter the value of index: %d\n", i);
+   //    scanf("%d", &ptr[i]);
+   // }
+   
+   // for (int i = 0; i < 5; i++) {
+   //    printf("The value of index %d is: %d\n", i,ptr[i]);
+   // }
+   
+   
+   ptr = (int*) calloc(3,sizeof(int));
+   for (int i = 0; i < 3; i++) {
+      printf("Enter the value of index: %d\n", i);
+      scanf("%d", &ptr[i]);
+   }
+   
+   for (int i = 0; i < 3; i++) {
+      printf("The value of index %d is: %d\n", i,ptr[i]);
+   }
+   
+   
+   ptr = (int*) realloc(ptr,5*sizeof(int));
+   for (int i = 0; i < 5; i++) {
+      printf("Enter the value of index: %d\n", i);
+      scanf("%d", &ptr[i]);
+   }
+   
+   for (int i = 0; i < 5; i++) {
+      printf("The value of index %d is: %d\n", i,ptr[i]);
+   }
+   
+   free(ptr);
+   
+   ptr = (int*) malloc(3*sizeof(int));
+   for (int i = 0; i < 5; i++) {
+      printf("Enter the value of index: %d\n", i);
+      scanf("%d", &ptr[i]);
+   }
+   
+   for (int i = 0; i < 5; i++) {
+      printf("The value is : %d\n", ptr[i]);
+      // scanf("%d", &ptr[i]);
+   }
+      
    return 0;
 };
